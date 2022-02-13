9cff908c12d75c0af32920358312542e697be714
Ezequiel-Lima <joao.lima94@fatec.sp.gov.br>
2022-02-13 12:56:30 -0300

    Add project files.

---
+++ AulaBalta01POO/Program.cs
@@ -0,0 +1,19 @@
+﻿using AulaBalta01POO.ContentContext;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AulaBalta01POO
+{
+    internal class Program
+    {
+        static void Main(string[] argd)
+        {
+            var course = new Course();
+
+            Console.ReadKey();
+        }
+    }
+}
